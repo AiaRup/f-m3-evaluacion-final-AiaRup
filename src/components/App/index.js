c6dcb1cf6d9681c846adf8d0b0f385958238af60
AiaRup <aiale88@gmail.com>
2019-05-24 08:44:14 +0200

    Add fetch functionality, show list of characters in the page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,11 +5,41 @@ import { fetchCharacters } from '../../services/fetchCharacters';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      charactersList: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetcNewCharacters();
+  }
+
+  fetcNewCharacters() {
+    fetchCharacters().then(results => {
+      const newResults = results.map((item, index) => {
+        return { ...item, id: index };
+      });
+      this.setState({ charactersList: newResults });
+    });
   }
 
   render() {
-    return <div className="app" />;
+    const { charactersList } = this.state;
+    return (
+      <div className="page">
+        <ul className="page__list">
+          {charactersList.map(character => (
+            <div className="list__character">
+              <h2 className="character__title">{character.name}</h2>
+              <div className="character__image-container">
+                <img src={character.image} alt={character.name} className="character__image" />
+              </div>
+              <h3 className="character__house">{character.house}</h3>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
